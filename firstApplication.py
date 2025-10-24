0845546184a81b4b826dafa30f10a9b122cb0388
EugeniuszKleszcz <matizm754@gmail.com>
2025-10-24 17:53:10 +0000

    Total commit

--- firstApplication.py
+++ firstApplication.py
@@ -3,4 +3,5 @@ zmiennoprzecinkowa = 0.9
 napis = "Tekst"
 logiczna = False
 
+print("Wesołych świąt")
 print(f"liczba: {liczba}, liczba zmiennoprzecinkowa: {zmiennoprzecinkowa}, napis: {napis}, logiczna: {logiczna}. Siema Mateuszeq.")
